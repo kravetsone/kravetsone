4a84ac0c6a27f9e78878f600efc68288e6c8c88b
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-02-15 00:56:53 +0300

    feat: add SlideLogo component and express slide

---
+++ presentations/Elysia/components/SlideLogo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+    title: "ExpressJS"
+}>()
+
+const logos: Record<typeof props["title"], string> = {
+    ExpressJS: "/express-logo.png"
+}
+</script>
+
+<template>
+    <div class="flex flex-row items-center font-bold">
+        <img width="50px" :src="logos[props.title]" />
+        <span class="text-xl">{{ props.title }}</span>
+    </div>
+</template>
